f8c8393cfae47682f54881341d7187deb086ecd0
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-11 17:45:19 +0530

    refactor: add brand name on home page view

--- product/serializers/home_page.py
+++ product/serializers/home_page.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from ..models.product import Product, ProductPrice, ProductImage
+from ..models.brands import Brand
+
 
 class ProductPriceSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductPrice
         fields = ["mrp", "selling_price", "discount_rate"]
 
+class BrandSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ["name"]
 
 class HomeProductSerializer(serializers.ModelSerializer):
     price = ProductPriceSerializer(read_only=True)
+    brand = BrandSerializers(read_only=True)
     image = serializers.SerializerMethodField()
 
     class Meta:
