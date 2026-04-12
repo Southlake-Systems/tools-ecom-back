e031695796bbd24d55e2e18d73433daad4135622
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-13 04:34:06 +0530

    refactor: add new filed favourite to Brand model

--- brands/models.py
+++ brands/models.py
@@ -5,7 +5,8 @@ from django.db import models
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    logo = models.ImageField(upload_to="brands/")
+    image = models.ImageField(upload_to="brands/", null=True, blank=True)
+    favourite = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
