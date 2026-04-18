17303ed7e7261444a9eb099a63dffdce2ca2e036
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-18 05:53:23 +0530

    refactor: refactored the add prodcut api to section to handle bulk create

--- homepage/api/add_product.py
+++ homepage/api/add_product.py
@@ -1,16 +1,54 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+from product.models.product import Product
 from rest_framework import status
+from ..models import SectionProduct,HomeSection
 from ..serializers.create_homepage_serializer import AddProductsToSectionSerializer
 
 
 class AddProductsToSectionView(APIView):
 
     def post(self, request):
-        serializer = AddProductsToSectionSerializer(data=request.data)
+        data = request.data
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Products added successfully"})
+        section_id = data.get("section_id")
+        product_ids = data.get("product_ids", [])
 
-        return Response(serializer.errors, status=400)
+        if not isinstance(product_ids, list) or not product_ids:
+            raise ValidationError({"product_ids": "Must be a non-empty list"})
+
+        # Validate section
+        section = get_object_or_404(HomeSection, id=section_id)
+
+        # Fetch products
+        products = Product.objects.filter(id__in=product_ids)
+
+        found_ids = set(products.values_list("id", flat=True))
+        missing_ids = set(product_ids) - found_ids
+
+        if missing_ids:
+            raise ValidationError({
+                "product_ids": f"Invalid IDs: {list(missing_ids)}"
+            })
+
+        # Avoid duplicates (important)
+        existing = set(
+            SectionProduct.objects.filter(
+                section=section,
+                product__in=products
+            ).values_list("product_id", flat=True)
+        )
+
+        new_links = [
+            SectionProduct(section=section, product=product)
+            for product in products
+            if product.id not in existing
+        ]
+
+        SectionProduct.objects.bulk_create(new_links)
+
+        return Response({
+            "message": "Products linked to section successfully"
+    },status=200)
