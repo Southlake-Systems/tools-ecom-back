f120e8578628f1ea6b1e72715f532dd2e75cf2f4
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-21 03:28:08 +0530

    refactored model field

--- brands/models.py
+++ brands/models.py
@@ -8,6 +8,7 @@ class Brand(models.Model):
     image_original = models.ImageField(upload_to="brands/original/", null=True, blank=True)
     image_medium = models.ImageField(upload_to="brands/medium/", null=True, blank=True)
     image_thumbnail = models.ImageField(upload_to="brands/thumbnail/", null=True, blank=True)
+    is_processed = models.BooleanField(default=False)
     favourite = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
 
