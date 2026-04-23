6193ce181eeba597ddd253cf57719074b3e161d4
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-23 06:42:16 +0530

    Feat: add URL 'images/upload/' for uploading product images

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,10 @@ from .api.add_product import AddProduct
 from .api.generate_product_id import GetProdID
 from .excel.product_bulk_upload import BulkProductUpload
 from .api.get_all_product import HomePageView
+from .api.upload_image import BulkUploadProductImages
 urlpatterns = [
     path('add/',AddProduct.as_view(), name="add product"),
+    path('images/upload/',BulkUploadProductImages.as_view(), name="upload product image"),
     path('get/id',GetProdID.as_view(), name="get product id"),
     path('bulk-upload/',BulkProductUpload.as_view(), name="bulk-upload"),
     path('get/all',HomePageView.as_view(), name="home-page-view"),
