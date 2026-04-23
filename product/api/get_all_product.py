c860c908086581ef61c36b007877e6c963abf513
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-23 06:42:47 +0530

    Refactor: Api not in use anymore

--- product/api/get_all_product.py
+++ product/api/get_all_product.py
@@ -1,21 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models import Prefetch
-
-from ..models.product import Product, ProductImage, ProductPrice
-from ..serializers.home_page import HomeProductSerializer
 
 
 class HomePageView(APIView):
 
     def get(self, request):
 
-        queryset = Product.objects.all().select_related("price").prefetch_related(
-            Prefetch("images", queryset=ProductImage.objects.filter(position=1, quality="LOW"))
-        )
 
-        serializer = HomeProductSerializer(queryset, many=True)
+        serializer = "Not in use anymore"
 
         return Response({
-            "response": serializer.data
+            "response": serializer
         })
