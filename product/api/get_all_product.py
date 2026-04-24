25722665b0f4d7b48848735445d4f9020f31a868
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-24 20:30:19 +0530

    Feat : added  product/<id> for product page

--- product/api/get_all_product.py
+++ product/api/get_all_product.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from ..serializers.product_page_serializer import ProductPageSerializer
+from ..models.product import Product
 
+from django.shortcuts import get_object_or_404
 
-class HomePageView(APIView):
+class ProductPageView(APIView):
 
-    def get(self, request):
+    def get(self, request, product_id):
 
+        product = get_object_or_404(Product, id=product_id)
 
-        serializer = "Not in use anymore"
+        serializer = ProductPageSerializer(product)
 
         return Response({
-            "response": serializer
+            "response": serializer.data
         })
