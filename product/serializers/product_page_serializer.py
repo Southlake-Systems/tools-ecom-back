c960e07ff7b0adfc19dd36e7d049e65b02cff17c
a.d.devnandan <a.d.devnandan@gmail.com>
2026-04-24 20:33:57 +0530

    Feat : added product/ for product page

---
+++ product/serializers/product_page_serializer.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+from ..models.product import Product, ProductPrice, ProductImage
+from brands.models import Brand
+
+
+
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductPrice
+        fields = ["mrp", "selling_price", "discount_rate"]
+
+class BrandSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = ["name"]
+
+class ProductPageSerializer(serializers.ModelSerializer):
+    price = ProductPriceSerializer(read_only=True)
+    brand = BrandSerializers(read_only=True)
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "brand",
+            "price",
+            "image",
+        ]
+
+    def get_image(self, obj):
+
+        request = self.context.get("request")
+
+        # first product image
+        product_image = obj.images.first()
+
+        if not product_image:
+            return None
+
+        # get MID quality variant
+        variant = product_image.variants.filter(
+            quality="MID"
+        ).first()
+
+        # fallback to original image
+        if variant and variant.file:
+            url = variant.file.url
+
+        elif product_image.original:
+            url = product_image.original.url
+
+        else:
+            return None
+
+        if request:
+            return request.build_absolute_uri(url)
+
+        return url
--- product/urls.py
+++ product/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path,include
 from .api.add_product import AddProduct
 from .api.generate_product_id import GetProdID
-from .excel.product_bulk_upload import BulkProductUpload
-from .api.get_all_product import HomePageView
+from .excel.product_bulk_upload import BulkUploadProducts
+from .api.get_all_product import ProductPageView
 from .api.upload_image import BulkUploadProductImages
+from .api.product_image_update import ProductImageDetailAPIView
+
 urlpatterns = [
     path('add/',AddProduct.as_view(), name="add product"),
     path('images/upload/',BulkUploadProductImages.as_view(), name="upload product image"),
     path('get/id',GetProdID.as_view(), name="get product id"),
-    path('bulk-upload/',BulkProductUpload.as_view(), name="bulk-upload"),
-    path('get/all',HomePageView.as_view(), name="home-page-view"),
+    path('bulk-upload/',BulkUploadProducts.as_view(), name="bulk-upload"),
+    path("product-images/<int:id>/",ProductImageDetailAPIView.as_view()),
+    path('<int:product_id>/',ProductPageView.as_view(), name="home-page-view"),
     
 
 
